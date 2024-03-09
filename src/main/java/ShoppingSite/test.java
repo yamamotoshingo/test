160736b5cdd550949a83ff8d58041078b9d35782
Yamamoto Shingo <Yamamoto Shingo@DESKTOP-3GKORB0>
2024-03-09 19:08:24 +0900

    commit test1900

--- src/main/java/ShoppingSite/test.java
+++ src/main/java/ShoppingSite/test.java
@@ -3,7 +3,11 @@ package ShoppingSite;
 public class test {
   int i = 100;
   String str = "今日は充実できたかな？";
-  for(i = 0; i < 10; i++){
+  
+  void test() {
+  for(i = 0;i < 10; i++){
     i += 1;
   }
+  }
+  
 }
