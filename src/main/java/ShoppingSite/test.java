56858c86a9877d38590e1a626f7039cd6153faf1
Yamamoto Shingo <Yamamoto Shingo@DESKTOP-3GKORB0>
2024-05-06 13:42:45 +0900

    20240506 山本 修正

--- src/main/java/ShoppingSite/test.java
+++ src/main/java/ShoppingSite/test.java
@@ -1,13 +1,20 @@
 package ShoppingSite;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class test {
   int i = 100;
   String str = "今日は充実できたかな？";
   Integer num = 0;
   
+  List<Integer> list = new ArrayList<>();
+  
   void test() {
   for(i = 0;i < 10; i++){
     i += 1;
+    list.add(i);
+    System.out.println(list);
   }
   }
   
